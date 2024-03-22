12dfb67dd5b1bb9871e6383a76646e98eae10ff4
Bakhtawar khan <160532125+baki1996@users.noreply.github.com>
2024-03-22 19:24:05 +0500

    Add files via upload

---
+++ task61.ts
@@ -0,0 +1,13 @@
+//Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, 
+//and motorcycles using enums, and show one example.
+
+
+// Making a list (enum) for different types of vehicles
+enum VehicleType {
+    Car,
+    Truck,
+    Motorcycle
+}
+
+// Showing one type of vehicle from the list
+console.log(VehicleType.Car); 
---
+++ task62.js
@@ -0,0 +1,11 @@
+//Making a Student Template: Create a blueprint for student information, including their name, age, 
+//and the classes they're taking, and then fill in this blueprint with an example student.
+// Filling in the blueprint with an example student
+var student = {
+    name: "Bakhtawar",
+    age: 25,
+    courses: ["Math", "Science", "History"]
+};
+// Showing the student's information
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
